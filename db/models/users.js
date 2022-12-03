cd9494c058ee0c05b3326054aa835c920ccfd9aa
alexesca <alexescamore@gmail.com>
2022-12-03 14:43:26 -0700

    Updated user swagger and schema fields

--- db/models/users.js
+++ db/models/users.js
@@ -5,10 +5,6 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     email: String,
     name: String,
-    phoneNumber: String,
-    date: Date,
-    username: String,
-    password: String
 });
 
 const UserModel = mongoose.model('User', UserSchema);
--- swagger.js
+++ swagger.js
@@ -2,44 +2,35 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
-        title: 'My API',
-        description: 'Description',
+        title: 'My Developer Productivity API',
+        description: 'This is the best productivity app.',
     },
     host: "localhost:3000",
     // host: "scripts-io.onrender.com",
     swaggerUIPath: '/api-docs',
     schemes: ['http'],
     // schemes: ['https'],
-    "securityDefinitions": {
-        "oauth": {
-            "type": "oauth2",
-            "authorizationUrl": "https://scripts-io.onrender.com/login/github",
-
-        }
-    },
     definitions: {
         Users: [{
             "email" : "m@gmail.com",
             "name" : "Miguel",
-            "phoneNumber" : "208-109-8899",
         }],
         User: {
             "email" : "m@gmail.com",
             "name" : "Miguel",
-            "phoneNumber" : "208-109-8899",
         },
-        Technologies: [{
+        Tasks: [{
             "_id" : "6375c691fdc424f3222d320f",
             "name" : "Angular",
             "description" : "Front-framewor developed by google.",
             "logo" : "angular.jpg",
         }],
-        Technology: {
+        Task: {
             "name" : "Angular",
             "description" : "Front-framewor developed by google.",
             "logo" : "angular.jpg",
         },
-        Scripts: [{
+        Sessions: [{
             "_userId" : "6375c50e3b3e912a359a2e35",
             "name" : "Create ng lib",
             "description" : "Creates an angular lib.",
@@ -47,7 +38,7 @@ const doc = {
             "date" : "2022-09-16T06:00:00.000+0000",
             "shortName" : "ng lib",
         }],
-        Script: {
+        Session: {
             "_userId" : "6375c50e3b3e912a359a2e35",
             "name" : "Create ng lib",
             "description" : "Creates an angular lib.",

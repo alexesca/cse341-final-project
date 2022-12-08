bafd19cb048e06dfa7f4f8efa8c7c670388aae3c
alexesca <alexescamore@gmail.com>
2022-12-07 21:37:07 -0700

    Added validate id middleware

---
+++ middlewares/validate-id.middleware.spec.js
@@ -0,0 +1,34 @@
+const middleware = require('./validate-id.middleware.js');
+
+describe('Validate id middleware', () => {
+    describe('Validate ID specs', validateIdSpecs);
+})
+
+
+function validateIdSpecs() {
+    it('should accept valid ID', function () {
+        const req = {
+            params: {
+                _id: '5b6d17206244a0f41a87dedf'
+            }
+        };
+        const res = {};
+        const next = (e) => {
+            expect(e).toBeUndefined();
+        }
+        middleware.validate(req, res, next)
+    });
+
+    it('should reject invalid ID', function () {
+        const req = {
+            params: {
+                _id: '123456789'
+            }
+        };
+        const res = {};
+        const next = (e) => {
+            expect(e).toBe("Invalid Mongo ID");
+        }
+        middleware.validate(req, res, next)
+    });
+}

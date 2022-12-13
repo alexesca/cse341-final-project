85b8f6138bacca5dc1f5ee464020f4076358f833
alexesca <alexescamore@gmail.com>
2022-12-13 15:13:16 -0700

    Added axios request to auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-var request = require("axios");
+const axios = require("axios");
 
 exports.create = async (req, res) => {
     // #swagger.tags = ['Auth']
@@ -13,22 +13,26 @@ exports.create = async (req, res) => {
     description: 'User successfully created.',
     schema: "Newly created User ID"
     } */
-    var options = { method: 'POST',
-        url: process.env.AUTH_URL,
-        headers: { 'content-type': 'application/json' },
-        json: true,
-        body: {
-        "client_id": process.env.AUTH_CLIENT_ID,
-            "client_secret": process.env.AUTH_CLIENT_SECRET,
-            "audience": process.env.AUTH_AUDIENCE,
-            "grant_type":"client_credentials"
+    try {
+        var config = {
+            method: 'post',
+            url: process.env.AUTH_URL,
+            headers: {
+                'content-type': 'application/json',
+                "Accept-Encoding": "gzip,deflate,compress"
+            },
+            data: {
+                "client_id": process.env.AUTH_CLIENT_ID,
+                "client_secret": process.env.AUTH_CLIENT_SECRET,
+                "audience": process.env.AUTH_AUDIENCE,
+                "grant_type":"client_credentials"
+            }
+        };
+        const response = await axios(config);
+        res.send(response.data);
+    } catch (e) {
+        debugger
+        throw e;
     }
-    };
-    return new Promise((resolve, reject) => {
-        request(options, function (error, response, body) {
-            if (error)  reject(error);
-            else resolve(body)
-        });
-    });
 };
 
